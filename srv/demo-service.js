2559e579b300064ca04e11427d518841210266c3
Milton Chandradas <milton.chandradas@live.com>
2022-06-07 22:24:21 -0400

    query language...

--- srv/demo-service.js
+++ srv/demo-service.js
@@ -1,7 +1,37 @@
 const cds = require("@sap/cds");
 
 module.exports = (srv) => {
+   const { Employees, Departments } = srv.entities;
+
    srv.before("*", (req) => {
       console.log(req.user.id);
+   });
+
+   srv.on("READ", Employees, async (req, next) => {
+      await next();
+
+      // return SELECT.from(Employees);
+   });
+
+   srv.on("READ", Departments, async (req, next) => {
+      await next();
+
+      // return SELECT.from(Departments);
+
+      // if (!req.query.SELECT.columns)
+      //    return await next();
+
+      // const expandIndex = req.query.SELECT.columns.findIndex(
+      //    ({ expand, ref }) => expand && ref[0] === "employees"
+      // );
+
+      // if (expandIndex < 0) return await next();
+
+      // return SELECT.from(Departments, (department) => {
+      //    department.name,
+      //       department.employees((employee) => {
+      //          employee.name;
+      //       });
+      // });
    });
 };
