742b1dddb25ea4fe602096b69950bbddab19f38d
Milton Chandradas <milton.chandradas@live.com>
2022-06-09 04:48:34 -0400

    user info...

--- srv/demo-service.js
+++ srv/demo-service.js
@@ -6,7 +6,18 @@ module.exports = (srv) => {
    const { Employees, Departments } = srv.entities;
 
    srv.before("*", (req) => {
-      console.log(req.user.id);
+      // let results = {};
+      // results.user = req.user.id;
+
+      // if (req.user.hasOwnProperty("locale")) {
+      //    results.locale = req.user.locale;
+      // }
+      // results.scopes = {};
+      // results.scopes.identified = req.user.is("identified-user");
+      // results.scopes.authenticated = req.user.is("authenticated-user");
+      // results.scopes.Admin = req.user.is("Admin");
+      // results.tenant = req.user.tenant;
+      // console.log("User Details: ", results);
    });
 
    srv.on("READ", Employees, async (req, next) => {
