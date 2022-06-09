3a3fd03ef73d7434ff8fb5048db41c819341ec2f
Milton Chandradas <milton.chandradas@live.com>
2022-06-09 04:40:35 -0400

    user info...

--- srv/demo-service.js
+++ srv/demo-service.js
@@ -2,6 +2,18 @@ const cds = require("@sap/cds");
 
 module.exports = (srv) => {
    srv.before("*", (req) => {
-      console.log(req.user.id);
+
+      let results = {};
+      results.user = req.user.id;
+
+      if (req.user.hasOwnProperty("locale")) {
+         results.locale = req.user.locale;
+      }
+      results.scopes = {};
+      results.scopes.identified = req.user.is("identified-user");
+      results.scopes.authenticated = req.user.is("authenticated-user");
+      results.scopes.Admin = req.user.is("Admin");
+      results.tenant = req.user.tenant;
+      console.log("User Details: ", results);
    });
 };
